ac29261a1db697440eb5540d908f171d89dac4a8
Lindsay Mulhollen <lindsaymulhollen@lindsays-mbp.attlocal.net>
2018-01-17 19:22:41 -0600

    loop doesn't output yet

---
+++ boybands.js
@@ -0,0 +1,23 @@
+let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+// Get a reference to the appropriate DOM element for bands
+const bandElement = document.getElementById("boy-bands");
+
+// Get a reference to the appropriate DOM element for vegetables
+const veggieElement = document.getElementById("vegetables");
+
+// Execute a for loop that will iterate over the arrays
+for (let loopTracker = 0; loopTracker < 5; loopTracker += 1) {
+
+    // Get a reference to the current item in the bands array
+    const currentBand = bands[loopTracker];
+
+    // Update the innerHTML value of the DOM element for bands
+    document.getElementById("boy-band").innerHTML = currentBand()
+    // Get a reference to the current item in the vegetables array
+    const currentVeggie = vegetables[loopTracker];
+
+    // Update the innerHTML value of the DOM element for vegetables
+    document.getElementById("vegetables").innerHTML = currentVeggie(" ")
+}
